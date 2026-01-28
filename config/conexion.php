5650f918825b60889ee1bc0d35ccefea0fb3aa39
Sebastián Alcoholado <sebaluinor@gmail.com>
2026-01-28 17:08:23 +0100

    Jerarquía cambiada a MVC de verdad

---
+++ config/conexion.php
@@ -0,0 +1,26 @@
+<?php
+class Database {
+
+    public static function conectar() {
+        try {
+            $host = "localhost";
+            $db   = "casino";
+            $user = "root";
+            $pass = "";
+
+            $pdo = new PDO(
+                "mysql:host=$host;dbname=$db;charset=utf8",
+                $user,
+                $pass
+            );
+
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+            return $pdo;
+
+        } catch (PDOException $e) {
+            die("Error de conexión: " . $e->getMessage());
+        }
+    }
+}
